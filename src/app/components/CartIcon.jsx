d5886c60b08588b7802ae57309f333e71e4c93ef
Gyandeep Aryan <gyandeeparyan.dev@gmail.com>
2024-08-16 20:31:12 +0530

    style: cartIcon  fixed

--- src/app/components/CartIcon.jsx
+++ src/app/components/CartIcon.jsx
@@ -4,7 +4,7 @@ import { ShoppingCart } from "lucide-react";
 const CartIcon = ({className}) => {
   const cartItems = 0;
   return (
-    <div className='flex px-2 py-1 cursor-pointer '>
+    <div className='flex py-1 cursor-pointer '>
       <ShoppingCart className="dark:text-gray-800" />
       <span className={`rounded-full h-6 w-6 px-2 text-center flex items-center justify-between -mt-2 bg-gray-800 text-white ${className}`}>
         {cartItems}
