f76aee74a4d2e601b58bdbc2b7042eb1f945b0ab
Gyandeep Aryan <gyandeeparyan.dev@gmail.com>
2024-08-16 20:29:47 +0530

    colors configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,22 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        mainDark: "hsl(var(--main-color-dark))",
+        accentDark: "hsl(var(--accent-color-dark))",
+        highlightDark: "hsl(var(--highlight-color-dark))",
+        textDark: "hsl(var(--text-color-dark))",
+        backgroundDark: "hsl(var(--background-color-dark))",
+        buttonDark: "hsl(var(--button-color-dark))",
+        cardDark: "hsl(var(--card-color-dark))",
+        mainLight: "hsl(var(--main-color-light))",
+        accentLight: "hsl(var(--accent-color-light))",
+        highlightLight: "hsl(var(--highlight-color-light))",
+        textLight: "hsl(var(--text-color-light))",
+        backgroundLight: "hsl(var(--background-color-light))",
+        buttonLight: "hsl(var(--button-color-light))",
+
+      },
     },
   },
   plugins: [],
