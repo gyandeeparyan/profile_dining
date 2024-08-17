090c680dde7e8a1701f4678015f1ddcd08817cf1
Gyandeep Aryan <gyandeeparyan.dev@gmail.com>
2024-08-17 08:37:59 +0530

    constants added & button component customised

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({className,onClick,text}) => {
   return (
     <button
     onClick={onClick}  
-    className={`rounded-lg px-4 py-2 bg-buttonLight dark:bg-buttonDark hover:bg-accentLight dark:hover:bg-red-500 dark:hover:text-white  px-7  text-sm font-semibold text-textLight  dark:textDark shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black ${className}`}>
+    className={`rounded-lg px-4 py-2 bg-buttonLight dark:bg-buttonDark hover:bg-accentLight dark:hover:bg-red-500 dark:hover:text-white    text-sm font-semibold text-textLight  dark:textDark shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black ${className}`}>
     {text} 
   </button>
   )
