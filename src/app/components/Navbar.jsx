3d7f31692886ac9b99b869ba19ee7df95a5c38bd
Gyandeep Aryan <gyandeeparyan.dev@gmail.com>
2024-08-18 16:36:44 +0530

    fix : navbar items bg-color linked to active route

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <div class='sticky hidden lg:block md:px-36  w-full bg-red-300 dark:bg-red-300'>
       <div class=' flex max-w-7xl items-center justify-between  py-2 sm:px-6 '>
         <div class="inline-flex items-center space-x-2 rounded-xl  px-4 py-2">
-     <Image  width={40} height={20} src={"https://p1.edge.duggup.com/logo/favicon.svg"}/>
+     <Image  width={40} alt="brand logo" height={20} src={"https://p1.edge.duggup.com/logo/favicon.svg"}/>
     </div>
         <div class='hidden grow items-start lg:flex'>
           <ul class=' inline-flex space-x-8'>
--- src/app/components/NavbarMobile.jsx
+++ src/app/components/NavbarMobile.jsx
@@ -1,17 +1,17 @@
 'use client'
 import React from 'react';
 import Link from "next/link";
-import { Menu,Home, X, Sun,Moon,  Utensils ,IndianRupee, MessageSquareText,Info,ShoppingCart } from 'lucide-react';
-import CartIcon from "./CartIcon"
-import { useSelector,useDispatch } from 'react-redux';
-import { toggleTheme } from '../lib/features/appSlice';
-import ThemeToggler from "@/app/components/themeToggler"
+import { usePathname } from 'next/navigation';
+import { Home, Utensils } from 'lucide-react';
+import CartIcon from "./CartIcon";
+import { useSelector, useDispatch } from 'react-redux';
+import ThemeToggler from "@/app/components/themeToggler";
 
+function NavbarMobile() {
+    const isDark = useSelector((store) => store.app.isDark);
+    const dispatch = useDispatch();
+    const pathname = usePathname();  // Get the current path
 
-
- function NavbarMobile() {
-    const isDark=useSelector((store)=>store.app.isDark)
-    const dispatch=useDispatch()
     const menuItems = [
         {
             name: 'Home',
@@ -26,7 +26,7 @@ import ThemeToggler from "@/app/components/themeToggler"
         {
             name: 'Theme',
             href: '',
-            icon: <ThemeToggler/>, 
+            icon: <ThemeToggler />,
         },
         {
             name: 'Cart',
@@ -35,24 +35,21 @@ import ThemeToggler from "@/app/components/themeToggler"
         },
     ];
 
-
-
-
     return (
-        <div className="sticky md:hidden z-50 bottom-0   bg-red-300  text-textLight  w-full ">
-            <div className="mx-auto  flex max-w-7xl items-center justify-between my-3 px-4 py-2 sm:px-6 lg:px-8">
+        <div className="sticky md:hidden z-50 bottom-0 bg-red-300 text-textLight w-full">
+            <div className="mx-auto flex max-w-7xl items-center justify-between my-3 px-4 py-2 sm:px-6 lg:px-8">
                 {menuItems.map((item) => (
-                    <Link  key={item.name} href={item.href}>
-                   <div className="flex-col  dark:focus:bg-rose-600 dark:hover:bg-rose-600 py-2 px-4 rounded-2xl dark:hover:text-mainDark dark:focus:text-mainDark  text-center justify-center items-center">
-                    <span className="items-center ">{item.icon}</span>
-                  
-                   </div>
-                   </Link>
+                    <Link key={item.name} href={item.href}>
+                        <div className={`flex-col py-2 px-4 rounded-2xl text-center justify-center items-center
+                            ${pathname === item.href ? 'bg-rose-500' : 'hover:bg-rose-600 dark:hover:bg-rose-600'}
+                            ${pathname === item.href ? 'dark:text-mainDark' : 'dark:hover:text-mainDark'}`}>
+                            <span className="items-center">{item.icon}</span>
+                        </div>
+                    </Link>
                 ))}
-                
             </div>
         </div>
     );
 }
 
-export default NavbarMobile
+export default NavbarMobile;
