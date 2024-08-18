edc006d5c0debbc42c63e2f9e246ea0ac2927fc6
Gyandeep Aryan <gyandeeparyan.dev@gmail.com>
2024-08-18 09:04:37 +0530

    dark mode stored in appSlice

--- src/app/lib/store.js
+++ src/app/lib/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./features/cartSlice";
 import resturantListingSlice from "./features/resturantListingSlice";
+import appSlice from "./features/appSlice"
 export function makeStore() {
   return configureStore({
     reducer: {
+      app:appSlice,
       cart: cartSlice,
       resturantListing: resturantListingSlice,
     },
